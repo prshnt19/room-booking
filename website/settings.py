f1c9079b25d0102700cbc740489ef08fcac2ddcb
poorvakhandare3006 <poorvakhandare1999@gmail.com>
2020-09-01 00:11:03 +0530

    working

--- website/settings.py
+++ website/settings.py
@@ -80,14 +80,11 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Form',
-        'USER': 'MDP',
-        'PASSWORD': 'Pras@9876',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 
